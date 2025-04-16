d823855f1d3fa251a6b68092bc289512bb37681e
Nataliia Tur <natalim@ukr.net>
2025-04-16 20:38:39 +0300

    fix

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-console.log("API Key on Vercel:", import.meta.env.VITE_API_KEY);
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTA3MWJhOTc4MWYxOWVkNWM2Mzk1MDc4OGRiYTgyNCIsIm5iZiI6MTc0NDQ4MDIwMS4xOTUwMDAyLCJzdWIiOiI2N2ZhYTdjOWEwNGY1MzBiNWM5OTYyZDAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.xLaMQmHsSDXD9eKNUizU2-yQnPTT0TDlpNLa_nG1WTA";
 
 const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
